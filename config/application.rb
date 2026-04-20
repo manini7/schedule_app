451501bd985ed5610e57c25e4fea1fe50de760bb
manini7 <maniniweb7@gmail.com>
2026-04-21 00:40:00 +0900

    項目ごとのエラーメッセージを実装しました

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module ScheduleApp
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.time_zone = "Tokyo"
-
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
   end
 end
